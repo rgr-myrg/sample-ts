253b612fd70e3e468a36a6a79fe6017dddfea99f
Roger <rpmyrg@gmail.com>
2018-01-12 23:37:26 -0500

    Added Mux first pass sample implementation.

--- src/main/ts/MuxBuilder.ts
+++ src/main/ts/MuxBuilder.ts
@@ -28,4 +28,24 @@ export class MuxBuilder {
 		});
 		return this;
 	}
+
+	public emitVideoChange(): MuxBuilder {
+		return this;
+	}
 }
+/*
+https://docs.mux.com/video-element/changing-the-video
+// Example of changing the source of a video element
+// Should happen before emitting the videochange event
+var myVideo = document.querySelector('#myVideo');
+myVideo.src = 'nextVideo.mp4';
+
+if (typeof mux !== 'undefined') {
+  mux.emit('#myVideo', 'videochange', {
+    video_id: 'abc345',
+    video_title: 'My Other Great Video',
+    video_series: 'Weekly Great Videos',
+    ...
+  });
+}
+*/
